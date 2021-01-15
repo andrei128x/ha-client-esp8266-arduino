7a2929f445357929a60a562606a3242265239009
Andrei Chelaru <andrei128@yahoo.com>
2021-01-15 11:06:19 +0200

    First commit for ESP32 branch\

---
+++ src/devices/esp32_adc.cpp
@@ -0,0 +1,35 @@
+/* FUNCTIONS unit */
+#include "../init/setup.h"
+#include "./header_file.h"
+
+/* ----------- DEFINES ------------- */
+#define SOME_LOCAL_DEFINE
+
+/*------------ VARIABLES -------------- */
+int some_variable_here = 42;
+
+/* ----------- FUNCTIONS -------------- */
+
+unsigned int some_function_here()
+{
+    return 42;
+}
+
+/**
+ * @brief 
+ * 
+ */
+class ESP32_ADC
+{
+    private:
+         bool bInitDone = false;
+
+    public:
+    
+    ESP32_ADC()
+    {
+        
+    }
+}
+
+/* ---END OF FILE --- */
---
+++ src/devices/esp32_adc.h
@@ -0,0 +1,23 @@
+/* SYSTEM unit */
+#ifndef _ESP32_ADC_H
+#define _ESP32_ADC_H
+
+#include <Arduino.h>
+#include "pins_arduino.h"
+#include "../init/global.h"
+
+/* ----------- DEFINES ------------- */
+#define SOME_DEFINE_HERE
+
+/* ----------- NOINIT VARIABLES sections ------------- */
+
+
+/* ----------- VARIABLES sections ------------- */
+extern int some_variable_here;
+
+/* ----------- FUNCTIONS section -------------- */
+extern unsigned int some_function_here();
+
+/* -- no code below this line */
+#endif
+/* ---END OF FILE --- */
